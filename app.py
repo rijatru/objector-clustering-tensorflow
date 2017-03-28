a2c1970c5cfc30d7bc810b4abb5062d3d9ebf472
rijatru <rijatru@gmail.com>
2017-03-28 14:10:41 -0500

    Tensor flow first commit after template init

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import numpy as np
 import tensorflow as tf
 from flask import Flask, jsonify, render_template, request
+from tensorflow_app import tf_method as method
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return 'OK!'
+    return method()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tensorflow_app.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+a = tf.constant([2])
+b = tf.constant([3])
+
+
+def tf_method():
+    return 'Todo bien perrito!'
